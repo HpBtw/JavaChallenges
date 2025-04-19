03244e91ddaa60f992c1b7e9d24a7d4af023410d
Luciano D'Oliveira <lucianohenriqueoliveira8@hotmail.com>
2025-04-19 20:00:10 -0300

    Scans a hh:mm:ss time and converts to seconds

--- src/challenge9/Main.java
+++ src/challenge9/Main.java
@@ -1,12 +1,25 @@
 package challenge9;
 
-// receive a hh:mm:ss and converts to seconds
+// Scans a hh:mm:ss time and converts to seconds
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int hr = 2;
-        int min = 10;
-        int sec = 50;
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Insert time (hh:mm:ss): ");
+        String time = kb.next();
+        if (time.length() != 8) {
+            System.err.println("Invalid time, use hh:mm:ss" +
+                    "\nexample: 02:10:05");
+            return;
+        }
+
+        char[] c = time.toCharArray();
+        int hr = Integer.parseInt(c[0] + "" + c[1]);
+        int min = Integer.parseInt(c[3] + "" + c[4]);
+        int sec = Integer.parseInt(c[6] + "" + c[7]);
 
         convSecs(hr, min, sec);
     }
@@ -15,6 +28,6 @@ public class Main {
         int mins = hr * 60 + min;
         int secs = mins * 60 + sec;
 
-        System.out.println(secs);
+        System.out.println(secs + " secs");
     }
 }
